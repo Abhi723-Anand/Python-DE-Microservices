4c7340cd9c02cfd8368f9b7e659d8c5fceb29cd6
Abhinav <7205527695.abhi@gmail.com>
2026-08-23 14:42:37 +0530

    Arrays DS

---
+++ Arrays.py
@@ -0,0 +1,8 @@
+# This list will store integer type elements
+arr = [1, 2, 3, 4, 5]
+
+# This list will store character type elements (strings in Python)
+arr = ['a', 'b', 'c', 'd', 'e']
+
+# This list will store float type elements
+arr = [1.4, 2.0, 24.0, 5.0, 0.0]  # All float values
