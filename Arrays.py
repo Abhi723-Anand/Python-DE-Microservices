ade8eda65ee26054f1db91b408ea0dcfef19cedc
Abhinav <7205527695.abhi@gmail.com>
2026-08-23 15:14:57 +0530

    Traversal for array

--- Arrays.py
+++ Arrays.py
@@ -6,3 +6,14 @@ arr = ['a', 'b', 'c', 'd', 'e']
 
 # This list will store float type elements
 arr = [1.4, 2.0, 24.0, 5.0, 0.0]  # All float values
+
+#Forward Traversal of an Array
+arr = [1, 2, 3, 4, 5]
+for i in range(len(arr)):
+    print(arr[i])  # Output: 1 2 3 4 5
+
+#Reverse Traversal of an Array
+arr = [1, 2, 3, 4, 5]
+for i in range(len(arr) - 1, -1, -1):
+    print(arr[i])  # Output: 5 4 3 2 1
+    
